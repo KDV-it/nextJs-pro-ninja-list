00ab4dc860628cbf5d8f4d1ef44a8dfee12f7f6a
KDV-it <voduykhang312001@gmail.com>
2022-06-07 19:23:36 +0700

    change some character to lowercase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../comps/Footer'
-import Navbar from '../comps/NavBar'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
